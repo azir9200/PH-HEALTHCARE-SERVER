a25efe6e484f06afcfe78445340834af4a3c2fa5
Azir@9200 <aziruddin83@hotmail.com>
2025-04-05 13:15:51 +0100

    url added

--- dist/app.js
+++ dist/app.js
@@ -1,7 +1,17 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
@@ -11,6 +21,13 @@ const globalErrorHandler_1 = __importDefault(require("./app/middlewares/globalEr
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
 const appointment_service_1 = require("./app/modules/Appointment/appointment.service");
 const node_cron_1 = __importDefault(require("node-cron"));
+const dotenv_1 = __importDefault(require("dotenv"));
+// Load environment variables
+dotenv_1.default.config();
+// Configure CORS dynamically
+const allowedOrigins = ((_a = process.env.CORS_ORIGINS) === null || _a === void 0 ? void 0 : _a.split(",")) || [
+    "http://localhost:3000",
+];
 const app = (0, express_1.default)();
 // CORS
 app.use((0, cors_1.default)({
@@ -21,32 +38,31 @@ app.use((0, cors_1.default)({
     ],
     credentials: true,
 }));
-
 app.use((0, cookie_parser_1.default)());
 // Body parsers
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
 app.options("*", (0, cors_1.default)());
-node_cron_1.default.schedule("* * * * *", () => {
+node_cron_1.default.schedule("* * * * *", () => __awaiter(void 0, void 0, void 0, function* () {
     try {
         appointment_service_1.AppointmentService.cancelUnpaidAppointments();
     }
     catch (err) {
-        console.error(err);
+        console.error("Error canceling unpaid appointments:", err);
     }
-});
+}));
 // Root route
 app.get("/", (req, res) => {
     res.send({
-        message: "Ph health care server..",
+        message: "Ph Health Care Server is running...",
     });
 });
 // API routes
 app.use("/api/v1", routes_1.default);
 // Handle 404 errors before the global error handler
-app.use((req, res) => {
-    res.status(http_status_1.default.NOT_FOUND).json({
-        success: false,
+app.all("*", (req, res, next) => {
+    next({
+        status: http_status_1.default.NOT_FOUND,
         message: "API Not Found!",
         error: {
             path: req.originalUrl,
--- dist/app/middlewares/globalErrorHandler.js
+++ dist/app/middlewares/globalErrorHandler.js
@@ -1,8 +1,38 @@
 "use strict";
+// import { Prisma } from "@prisma/client";
+// import { NextFunction, Request, Response } from "express";
+// import httpStatus from "http-status";
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+// const globalErrorHandler = (
+//   err: any,
+//   req: Request,
+//   res: Response,
+//   next: NextFunction
+// ) => {
+//   let statusCode = httpStatus.INTERNAL_SERVER_ERROR;
+//   let success = false;
+//   let message = err.message || "Something went wrong!";
+//   let error = err;
+//   if (err instanceof Prisma.PrismaClientValidationError) {
+//     message = "Validation Error";
+//     message = error.name;
+//     error = err.message;
+//   } else if (err instanceof Prisma.PrismaClientKnownRequestError) {
+//     if (err.code === "P2002") {
+//       message = "Duplicate Key error";
+//       error = err.meta;
+//     }
+//   }
+//   res.status(statusCode).json({
+//     success,
+//     message,
+//     error,
+//   });
+// };
+// export default globalErrorHandler;
 const client_1 = require("@prisma/client");
 const http_status_1 = __importDefault(require("http-status"));
 const globalErrorHandler = (err, req, res, next) => {
@@ -10,20 +40,21 @@ const globalErrorHandler = (err, req, res, next) => {
     let success = false;
     let message = err.message || "Something went wrong!";
     let error = err;
-    // if (err instanceof Prisma.PrismaClientKnownRequestError) {
-    //   message = "Validation Error";
-    //   message = error.name;
-    //   error = err.message;
-    // }
+    // Handle Prisma validation errors
     if (err instanceof client_1.Prisma.PrismaClientValidationError) {
         message = "Validation Error";
-        message = error.name;
+        message = "Validation Error";
         error = err.message;
     }
+    // Handle unique constraint errors (e.g., duplicate key)
     else if (err instanceof client_1.Prisma.PrismaClientKnownRequestError) {
         if (err.code === "P2002") {
-            message = "Duplicate Key error";
+            message = "Validation Error";
+            message = "Duplicate Key Error";
             error = err.meta;
+        }
+        else {
+            message = err.message;
         }
     }
     res.status(statusCode).json({
